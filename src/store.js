68935534ca9025c7ca46f92b522b4641d9160b8d
nvdhau <nvdhau@gmail.com>
2020-04-11 19:10:31 -0700

    added redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 // reduct-persist
 import { persistReducer } from 'redux-persist';
 import storage from  'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { todos } from './todos/reducers';
 
 const reducers = {
@@ -24,6 +26,7 @@ export const configureStore = () =>
   createStore(
     persistedReducer,
     // connect to redux dev tools extension
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeWithDevTools(
+      applyMiddleware(thunk)
+    ),
   );
