50e98d1078c912ba8706327a28a3ac7f9ca840ce
nvdhau <nvdhau@gmail.com>
2020-04-11 17:52:28 -0700

    setup redux-dev-tool

--- src/store.js
+++ src/store.js
@@ -1,4 +1,8 @@
 import { createStore, combineReducers } from 'redux';
+// reduct-persist
+import { persistReducer } from 'redux-persist';
+import storage from  'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { todos } from './todos/reducers';
 
 const reducers = {
@@ -7,4 +11,19 @@ const reducers = {
 
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+// reduct-persist
+const persistConfig = {
+  key: 'root',
+  storage, // local storage of the web
+  stateReconciler: autoMergeLevel2, // how depth 
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => 
+  // createStore(rootReducer);
+  createStore(
+    persistedReducer,
+    // connect to redux dev tools extension
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
