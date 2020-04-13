44477eb9344cc79921dc9b19fa1246ec037bc27e
nvdhau <nvdhau@gmail.com>
2020-04-12 23:31:46 -0700

    reuse style in styled-components

--- src/todos/ToDoListItem.js
+++ src/todos/ToDoListItem.js
@@ -4,15 +4,18 @@ import styled from 'styled-components';
 const ToDoItemContainer = styled.div`
   background: #fff;
   border-radius: 8px;
+  margin-top: 8px;
+  padding: 16px;
+  position: relative;
+  box-shadow: 0 4px 8px grey;
+`;
+
+const ToDoItemContainerWithWarning = styled(ToDoItemContainer)`
   border-bottom: ${props => 
     (new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
       ? 'none'
       : '2px solid red'
     )};
-  margin-top: 8px;
-  padding: 16px;
-  position: relative;
-  box-shadow: 0 4px 8px grey;
 `;
 
 const ButtonContainer = styled.div`
@@ -21,7 +24,7 @@ const ButtonContainer = styled.div`
   bottom: 12px;
 `;
 
-const CompletedButton = styled.button`
+const Button = styled.button`
   font-size: 16px;
   padding: 8px;
   border: none;
@@ -29,24 +32,23 @@ const CompletedButton = styled.button`
   outline: none;
   cursor: pointer;
   display: inline-block;
+`;
+
+const CompletedButton = styled(Button)`
   background-color: #22ee22;
 `;
 
-const RemoveButton = styled.button`
-  font-size: 16px;
-  padding: 8px;
-  border: none;
-  border-radius: 8px;
-  outline: none;
-  cursor: pointer;
-  display: inline-block;
+const RemoveButton = styled(Button)`
   background-color: #ee2222;
   margin-left: 8px;
 `;
 
 const ToDoListItem = ({ todo, onRemovePressed, onMarkAsDonePressed }) => {
+
+  const Container = todo.isCompleted ? ToDoItemContainer : ToDoItemContainerWithWarning;
+
   return ( 
-    <ToDoItemContainer createdAt={todo.createdAt}>
+    <Container createdAt={todo.createdAt}>
       <h3>{ todo.text }</h3>
       <p>
         Created at:&nbsp;
@@ -67,7 +69,7 @@ const ToDoListItem = ({ todo, onRemovePressed, onMarkAsDonePressed }) => {
           Remove
         </RemoveButton>
       </ButtonContainer>
-    </ToDoItemContainer>
+    </Container>
   );
 }
 
