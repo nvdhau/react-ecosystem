4d4e6898ec1f88c471ccacee8a505fa268983573
nvdhau <nvdhau@gmail.com>
2020-04-13 01:39:55 -0700

    test selector completedToDos

---
+++ src/todos/tests/selectors.test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { getCompletedToDos } from "../selectors";
+
+describe('The getCompletedToDos selector', () => {
+  it('Return only completed todos', () => {
+
+    const fakeTodos = [{
+      text: '1',
+      isCompleted: true,
+    },{
+      text: '2',
+      isCompleted: false,
+    },{
+      text: '3',
+      isCompleted: false,
+    }];
+
+    const expected = [{
+      text: '1',
+      isCompleted: true,
+    }];
+
+    const actual = getCompletedToDos.resultFunc(fakeTodos);
+
+    expect(actual).to.deep.equal(expected);
+  });
+});
